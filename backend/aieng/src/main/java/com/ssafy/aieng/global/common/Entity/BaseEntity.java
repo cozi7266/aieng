938f874ac03e174d0e291e1dfe280790c47e699c
Kyung Suk Lee <cozi7266@gmail.com>
2025-05-07 13:35:58 +0900

    Merge branch 'feat/BE-S12P31B104-60-api-kakao-login' of https://lab.ssafy.com/s12-final/S12P31B104 into feat/BE-S12P31B104-60-api-kakao-login
    # Conflicts:
    #	backend/aieng/src/main/java/com/ssafy/aieng/domain/auth/service/OAuthService.java

--- backend/aieng/src/main/java/com/ssafy/aieng/global/common/Entity/BaseEntity.java
+++ backend/aieng/src/main/java/com/ssafy/aieng/global/common/Entity/BaseEntity.java
@@ -5,9 +5,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -15,17 +12,15 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     protected Integer id;
 
-    @CreatedDate
     @Column(nullable = false, updatable = false)
     protected LocalDateTime createdAt;
 
-    @LastModifiedDate
     @Column(nullable = false)
     protected LocalDateTime updatedAt;
 
