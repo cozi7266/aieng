da3d6a626501d1394515331747b493522056fa50
minsang lee <alstkd2919@naver.com>
2025-05-13 13:36:08 +0900

    Revert "Feat: 퀴즈 조ê±ì생성 조건 확인 기능 구현 S12P31B104-129-5"
    This reverts commit 75668089ce4b0e7fd2df7051d5cf8c867f79d145.
    
    # Conflicts:
    #	backend/aieng/src/main/java/com/ssafy/aieng/domain/learning/repository/LearningRepository.java

--- backend/aieng/src/main/java/com/ssafy/aieng/domain/learning/dto/request/SentenceTTSRequest.java
+++ backend/aieng/src/main/java/com/ssafy/aieng/domain/learning/dto/request/SentenceTTSRequest.java
@@ -1,12 +1,21 @@
 package com.ssafy.aieng.domain.learning.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.*;
 
+@NoArgsConstructor  // 기본 생성자
+@AllArgsConstructor // 모든 필드를 포함하는 생성자
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class SentenceTTSRequest {
+    private Integer childId;
+    private Integer wordId;
+    private String sentence;
 
+    // 기본 생성자가 필요하다면 추가
+    // public SentenceTTSRequest() {}
 }
--- backend/aieng/src/main/java/com/ssafy/aieng/domain/learning/repository/LearningRepository.java
+++ backend/aieng/src/main/java/com/ssafy/aieng/domain/learning/repository/LearningRepository.java
@@ -5,11 +5,12 @@ import com.ssafy.aieng.domain.learning.entity.Learning;
 import com.ssafy.aieng.domain.learning.entity.Session;
 import com.ssafy.aieng.domain.word.entity.Word;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.domain.Pageable;
+
 
 import java.util.List;
 import java.util.Optional;
