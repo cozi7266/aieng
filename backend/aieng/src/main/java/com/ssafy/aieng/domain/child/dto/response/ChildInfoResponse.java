343754695cea7e501398b76b1e9c5e47e23347a3
Kyung Suk Lee <cozi7266@gmail.com>
2025-05-14 17:39:07 +0900

    fix : parentId -> userId 로 수정 및 코드 리팩토링

--- backend/aieng/src/main/java/com/ssafy/aieng/domain/child/dto/response/ChildInfoResponse.java
+++ backend/aieng/src/main/java/com/ssafy/aieng/domain/child/dto/response/ChildInfoResponse.java
@@ -12,15 +12,15 @@ import java.time.LocalDateTime;
 @Builder
 public class ChildInfoResponse {
 
-    private Integer parentId;
+    private Integer userId;
     private Integer childId;
     private String childName;
     private String childGender;
     private LocalDate childBirthday;
 
-    public static ChildInfoResponse of(User parentUser, Child child) {
+    public static ChildInfoResponse of(User User, Child child) {
         return new ChildInfoResponse(
-                parentUser.getId(),
+                User.getId(),
                 child.getId(),
                 child.getName(),
                 child.getGender().toString(),
